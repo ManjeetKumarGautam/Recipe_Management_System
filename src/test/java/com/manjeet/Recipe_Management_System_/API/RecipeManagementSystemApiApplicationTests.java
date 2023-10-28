c6c54ee033604547f0af67538eeb22b64e7d9564
kakarot <manjeetkumar1572000@gmail.com>
2023-10-28 15:21:05 +0530

    Push Recipe management System

---
+++ src/test/java/com/manjeet/Recipe_Management_System_/API/RecipeManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjeet.Recipe_Management_System_.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
